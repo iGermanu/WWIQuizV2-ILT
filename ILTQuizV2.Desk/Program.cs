00342def40f37433caaead5030acef2568a75a62
Vitor Germano <36730933+iGermanu@users.noreply.github.com>
2018-08-30 10:32:14 -0300

    jaaj
    jiroj

--- ILTQuizV2.Desk/Program.cs
+++ ILTQuizV2.Desk/Program.cs
@@ -16,7 +16,7 @@ namespace ILTQuizV2.Desk
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new RegisterUser());
         }
     }
 }
---
+++ ILTQuizV2.Desk/RegisterUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ILTQuizV2.Desk
+{
+    public partial class RegisterUser : Form
+    {
+        public RegisterUser()
+        {
+            InitializeComponent();
+        }
+
+        private void LblRegister_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            PnlLogin.Visible = false;
+            PnlRegister.Visible = true;
+            LblRegister.Visible = false;
+            LblLogar.Visible = true;
+        }
+
+        private void LblLogar_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            PnlRegister.Visible = false;
+            PnlLogin.Visible = true;
+            LblRegister.Visible = true;
+            LblLogar.Visible = false;
+        }
+    }
+}
